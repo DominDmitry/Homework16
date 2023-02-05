6f57dfad2b7b953bb87de4296af145f46d7b55da
DominDmitry <112818952+DominDmitry@users.noreply.github.com>
2023-02-05 13:59:22 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const dNoneEl = document.querySelector('.d-none');
+const divEl = document.querySelector('.js-div');
+const inputEl = document.querySelector('input');
+const text = document.createTextNode('we add <div>')
+
+inputEl.addEventListener('focus', () => {
+    divEl.classList.toggle('d-none'); 
+    divEl.appendChild(text);
+} );
+
+inputEl.addEventListener('blur', () => {
+    divEl.classList.toggle('d-none'); 
+} );
